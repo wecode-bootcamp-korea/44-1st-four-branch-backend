dfc74f1762c1a807697f6bb5ed7da9f0af12f76c
dahee jang <wol04255@gmail.com>
2023-04-03 16:49:14 +0900

    [ADD] Backend 초기 세팅

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./routes');
+const appDataSource = require('./models/appDataSource');
+const { globalErrorHandler } = require('./utils/error');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('combined'));
+app.use(routes);
+app.use(globalErrorHandler);
+
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
+app.get('/ping', function (req, res, next) {
+  res.status(200).json({ message: 'pong' });
+});
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
