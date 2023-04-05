6c9491950979f0a874284449b838aacbdb8dc85a
왕광현 <wang-kwanghyun@wang-gwanghyeon-ui-MacBookPro.local>
2023-04-05 21:06:31 +0900

    [WIP] 검색 기능 작업중3

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,7 @@ const { catchAsync } = require('../utils/error');
 const productSearch = catchAsync(async (req, res) => {
   const { name } = req.query;
   const search = await productService.searchProduct(name);
-  return res.status(200).json({ data: search });
+  return res.status(200).json({ search });
 });
 
 module.exports = {
--- models/productDao.js
+++ models/productDao.js
@@ -1,15 +1,24 @@
 const appdataSource = require('./appDataSource');
-// const connection = require('typeorm');
 
 const searchProduct = async (name) => {
   try {
-    await appdataSource.query(
+    const result = await appdataSource.query(
       `SELECT
+      id,
       name
-      FROM products
+      FROM products (
+        SELECT 
+          p.id,
+          images.url,
+          products_images.id
+          FROM products p 
+          JOIN products_images ON p.id = products_images.product_id 
+          JOIN images ON products_images.product_id = images.id 
+      )
       WHERE name LIKE '%${name}%'
       `
     );
+    return result;
   } catch (err) {
     const error = new Error('INVALID_DATA');
     console.log(err);
