c3fae4387d90bc53921831f5f133a3f8b7aaf446
왕광현 <wang-kwanghyun@wang-gwanghyeon-ui-MacBookPro.local>
2023-04-07 10:11:35 +0900

    [FIX] 검색기능 추가 리뷰 반영 수정3

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,11 +3,8 @@ const productController = require('../controllers/productController');
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get('', productController.productSearch);
-=======
 router.get('', productController.getProducts);
->>>>>>> main
 
 module.exports = {
   router,
