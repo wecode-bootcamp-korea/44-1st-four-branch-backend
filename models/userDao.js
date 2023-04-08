03b87641f2fb43ae3bd4848bb5df99c6cb16d737
dahee jang <wol04255@gmail.com>
2023-04-08 20:39:17 +0900

    [FIX] getUserById에서 password 제외

--- models/userDao.js
+++ models/userDao.js
@@ -65,7 +65,14 @@ const getUserById = async (userId) => {
   try {
     const [user] = await appDataSource.query(
       `SELECT
-        *
+        id,
+        first_name,
+        last_name,
+        email,
+        point,
+        agreements,
+        created_at,
+        updated_at
       FROM users
       WHERE id = ?`,
       [userId]
