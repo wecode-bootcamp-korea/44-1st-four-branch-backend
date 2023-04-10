39e0ae7679322cef120cb1a1ffb1b8458e4c3fc8
dahee jang <wol04255@gmail.com>
2023-04-10 18:56:21 +0900

    [FIX] point by pay 기능 - transaction(포인트 차감, order status update, carts 삭제)

--- models/orderDao.js
+++ models/orderDao.js
@@ -1,18 +1,40 @@
 const appDataSource = require('./appDataSource');
 
+//transaction 적용
 const payByPoint = async (orderNumber, userId) => {
+  const queryRunner = appDataSource.createQueryRunner();
+  await queryRunner.connect();
+  await queryRunner.startTransaction();
   try {
-    await appDataSource.query(
+    await queryRunner.query(
       ` UPDATE users u
         JOIN orders o ON o.user_id = u.id
         SET u.point = (u.point - o.total_price)
         WHERE o.number = ? AND o.user_id = ?`,
       [orderNumber, userId]
     );
+    await queryRunner.query(
+      `UPDATE orders o
+      JOIN users u ON o.user_id = u.id
+      SET o.status_id = 2
+      WHERE o.number = ? AND u.id = ?`,
+      [orderNumber, userId]
+    );
+
+    await queryRunner.query(
+      `DELETE FROM carts
+      WHERE user_id = ?`,
+      [userId]
+    );
+    await queryRunner.commitTransaction();
+    return;
   } catch (err) {
+    await queryRunner.rollbackTransaction();
     err.message = 'DATABASE_ERROR';
     err.statusCode = 400;
     throw err;
+  } finally {
+    await queryRunner.release();
   }
 };
 
