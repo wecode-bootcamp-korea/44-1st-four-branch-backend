6a83a0ea1565748e66160a086f695ae90e261f3f
왕광현 <wang-kwanghyun@wang-gwanghyeon-ui-MacBookPro.local>
2023-04-11 11:31:24 +0900

    [FIX] 주문정보저장 리뷰 반영 수정4

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -5,7 +5,6 @@ const loginReq = require('../utils/auth');
 const router = express.Router();
 
 router.post('', loginReq, orderController.createOrder);
-// router.post('/carts', loginReq, orderController.orderItems);
 router.get('', loginReq, orderController.orderInfo);
 
 module.exports = {
